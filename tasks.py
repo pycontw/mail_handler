2de2346dbe0f0aa8536510a5124d34922a86e263
LeeW <cl87654321@gmail.com>
2019-08-04 22:33:03 +0800

    build(tasks.py): add init, lint tasks

---
+++ tasks.py
@@ -0,0 +1,28 @@
+from invoke import task
+
+
+@task
+def init(cmd):
+    cmd.run('pipenv install --dev', pty=True)
+
+
+@task
+def pylint(cmd):
+    cmd.run('pipenv run pylint mail_renderer.py', pty=True)
+
+
+@task
+def flake8(cmd):
+    cmd.run('pipenv run flake8 mail_renderer.py', pty=True)
+
+
+@task
+def mypy(cmd):
+    cmd.run('pipenv run mypy mail_renderer.py', pty=True)
+
+
+@task
+def lint(cmd):
+    pylint(cmd)
+    flake8(cmd)
+    mypy(cmd)
