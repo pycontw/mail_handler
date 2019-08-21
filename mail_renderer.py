24d7a1c9f7a567b5de53fe79be006eebd8e8c480
LeeW <cl87654321@gmail.com>
2019-08-21 15:53:20 +0800

    refactor(mail_renderer.py): rename variables

--- mail_renderer.py
+++ mail_renderer.py
@@ -31,17 +31,17 @@ def export_mails(recv_to_mail, output_path):
 
 @click.command()
 @click.argument('template_path', type=click.Path(exists=True))
-@click.argument('receive_data_json', type=click.Path(exists=True))
+@click.argument('receiver_data', type=click.Path(exists=True))
 @click.option('--output_path', type=click.Path(exists=False), default='mails_to_sent', show_default=True)
-def main(template_path, receive_data_json, output_path):
+def main(template_path, receiver_data, output_path):
     if not os.path.isdir(output_path):
         logging.info('Create directory "%s"', output_path)
         os.mkdir(output_path)
 
-    with open(receive_data_json, 'r') as input_file:
-        receive_data = json.load(input_file)
-        common_data = receive_data['common_data']
-        unique_data = receive_data['unique_data']
+    with open(receiver_data, 'r') as input_file:
+        data = json.load(input_file)
+        common_data = data['common_data']
+        unique_data = data['unique_data']
 
     template = load_template(template_path)
     recv_to_mail = render_all_content(template, common_data, unique_data)
