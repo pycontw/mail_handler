6253a2c1bd0c64f06312edecee0ce2093f8cdd2b
Wei Lee <weilee.rx@gmail.com>
2020-05-27 15:01:55 +0800

    ci: run tox only on github actions instead of local
    Running tox is way too time consuming.
    We don't need to re-create virtual-env every time we run test locally.
    But it's good to have it before we merge it into master.

--- tasks/test.py
+++ tasks/test.py
@@ -6,7 +6,7 @@ from tasks.common import VENV_PREFIX
 @task(default=True)
 def run(ctx, allow_no_tests=False):
     """Run test cases"""
-    result = ctx.run(f"{VENV_PREFIX} tox", pty=True, warn=True)
+    result = ctx.run(f"{VENV_PREFIX} pytest", pty=True, warn=True)
     if allow_no_tests and result.exited == 5:
         exit(0)
     exit(result.exited)
