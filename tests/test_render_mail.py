ea21151c1bdc4e1feebb4c86142e35cc373b106e
allen91wu <allen91.wu@gmail.com>
2021-10-08 14:04:57 +0800

    test(unique_csv): add test case for testing csv click option

--- tests/test_render_mail.py
+++ tests/test_render_mail.py
@@ -12,6 +12,10 @@ from tests.utils import (
 
 path_j2 = "./templates/sponsorship/spam_sponsors_2020.j2"
 path_receivers_json = "./examples/sponsorship/spam_sponsors_2020.json"
+path_receivers_json_no_unique = (
+    "./examples/sponsorship/spam_sponsors_2020_no_unique.json"
+)
+path_unique_csv = "./examples/sponsorship/spam_sponsors_2020_unique_data.csv"
 path_pre_rendered_mails_no_separator = "./tests/data/no-separator"
 path_pre_rendered_mails_with_separator = "./tests/data/with-separator"
 
@@ -23,6 +27,32 @@ def test_rendered_mail_no_separator(all_mails_base_no_separator):
         [
             path_j2,
             path_receivers_json,
+            "--output_path",
+            path_mails_to_send_no_separator,
+        ],
+    )
+    all_mails_target = get_all_mail_names_from_path(
+        glob.glob("/".join((path_mails_to_send_no_separator, "*@*")))
+    )
+
+    assert result.exit_code == 0
+    assert len(all_mails_base_no_separator) == len(all_mails_target)
+    assert compare_rendered_mail_all(
+        all_mails_target,
+        base_prefix=path_pre_rendered_mails_no_separator,
+        target_prefix=path_mails_to_send_no_separator,
+    )
+
+
+def test_rendered_mail_no_separator_and_csv(all_mails_base_no_separator):
+    runner = CliRunner()
+    result = runner.invoke(
+        main,
+        [
+            path_j2,
+            path_receivers_json_no_unique,
+            "--unique_csv",
+            path_unique_csv,
             "--output_path",
             path_mails_to_send_no_separator,
         ],
@@ -48,6 +78,35 @@ def test_rendered_mail_with_separator_dash(all_mails_base_with_separator):
         [
             path_j2,
             path_receivers_json,
+            "--output_path",
+            path_mails_to_send_with_separator,
+            "--separator",
+            " - ",
+        ],
+    )
+
+    all_mails_target = get_all_mail_names_from_path(
+        glob.glob("/".join((path_mails_to_send_with_separator, "*@*")))
+    )
+
+    assert result.exit_code == 0
+    assert len(all_mails_base_with_separator) == len(all_mails_target)
+    assert compare_rendered_mail_all(
+        all_mails_target,
+        base_prefix=path_pre_rendered_mails_with_separator,
+        target_prefix=path_mails_to_send_with_separator,
+    )
+
+
+def test_rendered_mail_with_separator_dash_and_csv(all_mails_base_with_separator):
+    runner = CliRunner()
+    result = runner.invoke(
+        main,
+        [
+            path_j2,
+            path_receivers_json_no_unique,
+            "--unique_csv",
+            path_unique_csv,
             "--output_path",
             path_mails_to_send_with_separator,
             "--separator",
